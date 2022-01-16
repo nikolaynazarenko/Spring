9307cc1d2e7a96fc529d24c3f9d50cd298c37a9f
Nikolay Nazarenko <nazarenko.nikolay.93@icloud.com>
2022-01-16 18:30:42 +0300

    Test Passed

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,21 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+
+        HelloWorld secondBean =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(secondBean.getMessage());
+
+        Cat catBean =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(catBean.getName());
+
+        Cat secondCatBean =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(secondCatBean.getName());
+
+        System.out.println(catBean == secondCatBean);
+        System.out.println(bean == secondBean);
+
     }
 }
